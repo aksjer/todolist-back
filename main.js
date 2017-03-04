f07e5cdd33a0c370f6ec3f7f3447698b3474ecc0
aksjer <aksogut.jerome@gmail.com>
2017-03-04 16:27:15 +0100

    modification

--- main.js
+++ main.js
@@ -1,94 +1,78 @@
 const express = require('express'),
   app = express(),
-  mongoose = require('mongoose').connect('mongodb://localhost/todolist-api'),
+  mongoose = require('mongoose').connect('mongodb://localhost/todolist'),
   jwt = require('jsonwebtoken'),
   bodyParser = require('body-parser'),
-  secret = 'baddb054-1c6d-4872-bde8-ed0a5ada6f6d';
-
-const Todo = mongoose.model('todos', new mongoose.Schema({
-  id: Number,
-  title: String,
-  description: String,
-  status: Number,
-  date: Date
-}));
-
-const User = mongoose.model('users', new mongoose.Schema({
-  id: Number,
-  login: String,
-  password: String
-}));
+  secret = 'baddb054-1c6d-4872-bde8-ed0a5ada6f6d',
+  crypto = require('crypto'),
+  hash = crypto.createHash('sha256'),
+  Todos = mongoose.model('Todos', new mongoose.Schema({
+    id: Number,
+    title: String,
+    description: String,
+    status: Number,
+    date: Date
+  })),
+  Users = mongoose.model('users', new mongoose.Schema({
+    _id: Number,
+    user: String,
+    pwd: String,
+    roles: []
+  }));
 
 app
   .use(bodyParser.urlencoded({ extended: true }))
   .use(bodyParser.json())
   .use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    // res.setHeader("Access-Control-Allow-Credentials", "true");
-
-    // console.log(req.url)
-    // const token = req.headers.Authorization
-    // console.log(token)
-    // if (token) {
-    //   jwt.verify(token, secret, function (err, decoded) {
-    //     if (err)
-    //       return res.json({ success: false, err: err, code: 0 })
-    //     next();
-    //   })
-    // }
-    // else if (req.url === '/authenticate') {
-
-    // }
-    // else
-    //   return res.status(403).send({
-    //     success: false,
-    //     err: 'No token provided'
-    //   })
-
-
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
   .post('/authenticate', (req, res) => {
-    User.findOne({ id: req.body.id }, (err, user) => {
+    Users.findOne({ user: req.body.name }, (err, account) => {
       if (err)
         return res.json({ success: false, err: err, code: 0 });
-      if (!user)
+      if (!account)
         return res.json({ success: false, err: 'User not found', code: 1 });
-      if (user.password !== req.body.password)
+      hash.update(req.body.password)
+      if (account.pwd !== hash.digest('hex'))
         return res.json({ success: false, err: 'Incorrect password', code: 2 });
       return res.json({
         success: true,
-        user: user,
-        token: jwt.sign(user, secret, {
-          expiresIn: 1440 // expires in 24 hours
+        user: account.user,
+        token: jwt.sign(account, secret, {
+          expiresIn: 1440
         })
       })
     })
   })
   .get('/todo/:id', (req, res) => {
-    Todo.find({ id: req.params.id }, (err, data) => res.json(data));
+    Todos.find({ id: req.params.id }, (err, data) => res.json(data));
   })
-  .get('/todos', (req, res) => {
-    jwt.verify(req.headers.authorization, secret, (err, decoded) => {
-      if (err)
-        return res.json(err)
-      Todo.find({}, (err, data) => res.json(data));
-    })
+  .get('/todos', auth, (req, res) => {
+    Todos.find({}, (err, todos) => res.send(todos))
   })
   .post('/todo', (req, res, next) => {
-    Todo.create(req.body, (err, post) => {
+    Todos.create(req.body, (err, post) => {
       if (err) return next(err);
       res.json(post);
     })
   })
   .put('/todo/:id', (req, res) => {
-    Todo.update({ id: req.params.id }, req.body, (err, data) => {
+    Todos.update({ id: req.params.id }, req.body, (err, data) => {
       res.json(data);
     })
   })
   .delete('/todo/:id', (req, res) => {
-    Todo.remove({ id: req.params.id }, (err, data) => res.json(data));
+    Todos.remove({ id: req.params.id }, (err, data) => res.json(data));
   })
   .listen(3000);
+
+function auth(req, res, next) {
+  jwt.verify(req.headers.authorization, secret, (err, decoded) => {
+    if (err)
+      return res.status(403).send();
+    next();
+  });
+}
